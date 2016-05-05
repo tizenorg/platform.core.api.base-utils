95a56d64f7e274c1c151f20607cdf3818fb12d2a
Beata Stefaniuk <b.stefaniuk@samsung.com>
2016-05-05 10:00:02 +0200

    [Base-utils] MeasureFormat implementation
    Change-Id: I2506cc173afa9a91d5e1effe15d13fe75e385d57
    Signed-off-by: Beata Stefaniuk <b.stefaniuk@samsung.com>

---
+++ src/utils_i18n_measure_format.cpp
@@ -0,0 +1,188 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd All Rights Reserved
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#define __STDC_LIMIT_MACROS
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <utils_i18n_measure_format.h>
+#include <utils_i18n_private.h>
+#include <utils_i18n_ustring.h>
+
+#include <unicode/ustdio.h>
+#include <unicode/measfmt.h>
+#include <unicode/measunit.h>
+#include <unicode/format.h>
+#include <unicode/fmtable.h>
+#include <unicode/locid.h>
+#include <unicode/uchar.h>
+
+int i18n_measure_format_create(i18n_measure_format_h *measure_format, const char *language, const char *country, i18n_umeasure_width_format_e width_format)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+    Locale locale(language, country, 0, 0);
+
+    *measure_format = MeasureFormat::MeasureFormat(locale, width_format, status);
+    retv_if(measure_format == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    return _i18n_error_mapping(status);
+}
+/*
+int i18n_measure_format_create_with_number_format(i18n_measure_format_h *measure_format, const char *language, const char *country, i18n_umeasure_width_format_e width_format, i18n_number_format_h number_format)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+    Locale locale(language, country, 0, 0);
+
+    *measure_format = MeasureFormat::MeasureFormat(locale, width_format, (NumberFormat *) number_format, status);
+    retv_if(measure_format == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    return _i18n_error_mapping(status);
+}
+*/
+
+int i18n_measure_format_from_other(i18n_measure_format_h *measure_format, const i18n_measure_format_h other)
+{
+    retv_if(measure_format == NULL || other == NULL, I18N_ERROR_INVALID_PARAMETER);
+
+    *measure_format = MeasureFormat::MeasureFormat((MeasureFormat *) other);
+
+    return I18N_ERROR_NONE;
+}
+
+int i18n_measure_format_destroy(i18n_measure_format_h measure_format)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+
+    delete((MeasureFormat *) measure_format);
+
+    return I18N_ERROR_NONE;
+}
+
+int i18n_measure_format_clone(i18n_measure_format_h measure_format, i18n_format_h *clone)
+{
+    retv_if(measure_format == NULL || clone == NULL, I18N_ERROR_INVALID_PARAMETER);
+
+    *clone = ((MeasureFormat *) measure_format)->clone();
+
+    return I18N_ERROR_NONE;
+}
+
+int i18n_measure_format_format(i18n_measure_format_h measure_format, char **format_string, const i18n_formattable_h *formattable, char **appendTo, i18n_field_position_h field_position)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+    UnicodeString unicode_append_to(append_to);
+
+    UnicodeString result = ((MeasureFormat *) measure_format)->format((Formattable *) formattable, unicode_appendTo,
+                            (FieldPosition *) field_position, status);
+
+    const UChar *uchar_format = result.getTerminatedBuffer();
+    retv_if(uchar_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+
+    int32_t ulen = u_strlen(uchar_format);
+    retv_if(ulen <= 0, I18N_ERROR_INVALID_PARAMETER);
+
+    *format_string = (char *) malloc(ulen + 1);
+    retv_if(*format_string == NULL, I18N_ERROR_OUT_OF_MEMORY);
+    u_austrcpy(*format_string, uchar_format);
+
+    return _i18n_error_mapping(status);
+}
+
+int i18n_measure_format_parse_object(i18n_measure_format_h measure_format, char *source, const i18n_formattable_h *result, i18n_field_position_h field_position)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UnicodeString unicode_source(source);
+
+    ((MeasureFormat *) measure_format)->parseObject(unicode_source, *result, (FieldPosition *) field_position);
+
+    retv_if(*result == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    return I18N_ERROR_NONE;
+}
+
+/* FieldPosition object is not defined
+int i18n_measure_format_format_measures(i18n_measure_h *measures, i18n_measure_unit_h measure_unit, char **appendTo, i18n_field_position_h field_position)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+    UnicodeString unicode_append_to(append_to);
+
+    UnicodeString result = MeasureFormat::formatMeasurePerUnit(measures, (MeasureUnit *) measure_unit, unicode_appendTo,
+                            (FieldPosition *) field_position, status);
+
+    const UChar *uchar_format = result.getTerminatedBuffer();
+    retv_if(uchar_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+
+    int32_t ulen = u_strlen(uchar_format);
+    retv_if(ulen <= 0, I18N_ERROR_INVALID_PARAMETER);
+
+    *format_string = (char *) malloc(ulen + 1);
+    retv_if(*format_string == NULL, I18N_ERROR_OUT_OF_MEMORY);
+    u_austrcpy(*format_string, uchar_format);
+
+    return _i18n_error_mapping(status);
+}
+*/
+
+/* FieldPosition object is not defined
+int i18n_measure_format_format_measure_per_unit(i18n_measure_h measure, i18n_measure_unit_h measure_unit, char **appendTo, i18n_field_position_h field_position)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+    UnicodeString unicode_append_to(append_to);
+
+    UnicodeString result = MeasureFormat::formatMeasurePerUnit((Measure *) measure, (MeasureUnit *) measure_unit,
+                            unicode_appendTo, (FieldPosition *) field_position, status);
+
+    const UChar *uchar_format = result.getTerminatedBuffer();
+    retv_if(uchar_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+
+    int32_t ulen = u_strlen(uchar_format);
+    retv_if(ulen <= 0, I18N_ERROR_INVALID_PARAMETER);
+
+    *format_string = (char *) malloc(ulen + 1);
+    retv_if(*format_string == NULL, I18N_ERROR_OUT_OF_MEMORY);
+    u_austrcpy(*format_string, uchar_format);
+
+    return _i18n_error_mapping(status);
+}
+*/
+
+int i18n_measure_format_create_currency_format_from_locale(i18n_measure_format_h *measure_format, const char *language, const char *country)
+{
+    retv_if(measure_format == NULL || language == NULL || country == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+    Locale locale(language, country, 0, 0);
+
+    *measure_format = MeasureFormat::createCurrencyFormat(locale, status);
+    retv_if(*measure_format == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    return _i18n_error_mapping(status);
+}
+
+int i18n_measure_format_create_currency_format(i18n_measure_format_h *measure_format)
+{
+    retv_if((measure_format == NULL || language == NULL || country == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+
+    *measure_format = MeasureFormat::createCurrencyFormat(status);
+    retv_if(*measure_format == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    return _i18n_error_mapping(status);
+}
