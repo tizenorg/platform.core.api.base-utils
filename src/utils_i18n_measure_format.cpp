8a7331357e960b15b7071f06d74d5c8e3d38e94d
Beata Stefaniuk <b.stefaniuk@samsung.com>
2016-06-03 12:03:11 +0200

    [Base-utils][MeasureFormat] Missed functions added
    Change-Id: Idf590c5d73849266ea4afbabcdfa82e19d08c279
    Signed-off-by: Beata Stefaniuk <b.stefaniuk@samsung.com>

--- src/utils_i18n_measure_format.cpp
+++ src/utils_i18n_measure_format.cpp
@@ -28,6 +28,8 @@
 #include <unicode/measunit.h>
 #include <unicode/format.h>
 #include <unicode/fmtable.h>
+#include <unicode/fieldpos.h>
+#include <unicode/parsepos.h>
 #include <unicode/locid.h>
 #include <unicode/uchar.h>
 
@@ -72,6 +74,108 @@ int i18n_measure_format_clone(i18n_measure_format_h measure_format, i18n_format_
     retv_if(*clone == NULL, I18N_ERROR_OUT_OF_MEMORY);
 
     return I18N_ERROR_NONE;
+}
+
+int i18n_measure_format_format(i18n_measure_format_h measure_format, i18n_formattable_h formattable, char **append_to, i18n_field_position_h field_position)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(formattable == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(append_to == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(field_position == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+
+    UnicodeString unicode_append_to(*append_to);
+
+    UnicodeString result = ((MeasureFormat *) measure_format)->format((Formattable *) formattable, unicode_append_to, (FieldPosition &) field_position, status);
+    const UChar *uchar_result = result.getTerminatedBuffer();
+
+    retv_if(uchar_result == NULL, I18N_ERROR_INVALID_PARAMETER);
+    int32_t ulen = u_strlen(uchar_result);
+
+    retv_if(ulen <= 0, I18N_ERROR_INVALID_PARAMETER);
+    *append_to = (char *) malloc(ulen + 1);
+
+    retv_if(*append_to == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    u_austrcpy(*append_to, uchar_result);
+
+    return _i18n_error_mapping(status);
+}
+
+int i18n_measure_format_parse_object(i18n_measure_format_h measure_format, char *source, i18n_formattable_h *result, i18n_parse_position_h parse_position)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(source == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(result == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(parse_position == NULL, I18N_ERROR_INVALID_PARAMETER);
+
+    const UnicodeString unicode_source(source);
+
+    Formattable f = Formattable();
+    retv_if(&f == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    ((MeasureFormat *) measure_format)->parseObject(unicode_source, f, (ParsePosition &) parse_position);
+    retv_if(&f == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    *result = &f;
+    retv_if(*result == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    return I18N_ERROR_NONE;
+}
+
+int i18n_measure_format_format_measures(i18n_measure_format_h measure_format, i18n_measure_h measures, int32_t measure_count, char **append_to, i18n_field_position_h field_position)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(measures == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(measure_count <= 0, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(append_to == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(field_position == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+
+    UnicodeString unicode_append_to(*append_to);
+
+    UnicodeString result = ((MeasureFormat *) measure_format)->formatMeasures((const Measure *) measures, measure_count, unicode_append_to, (FieldPosition &) field_position, status);
+    const UChar *uchar_result = result.getTerminatedBuffer();
+
+    retv_if(uchar_result == NULL, I18N_ERROR_INVALID_PARAMETER);
+    int32_t ulen = u_strlen(uchar_result);
+
+    retv_if(ulen <= 0, I18N_ERROR_INVALID_PARAMETER);
+    *append_to = (char *) malloc(ulen + 1);
+
+    retv_if(*append_to == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    u_austrcpy(*append_to, uchar_result);
+
+    return _i18n_error_mapping(status);
+}
+
+int i18n_measure_format_format_measures_per(i18n_measure_format_h measure_format, i18n_measure_h measures, int32_t measure_count, i18n_measure_unit_h measure_unit, char **append_to, i18n_field_position_h field_position)
+{
+    retv_if(measure_format == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(measures == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(measure_count <= 0, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(measure_unit == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(append_to == NULL, I18N_ERROR_INVALID_PARAMETER);
+    retv_if(field_position == NULL, I18N_ERROR_INVALID_PARAMETER);
+    UErrorCode status = U_ZERO_ERROR;
+
+    UnicodeString unicode_append_to(*append_to);
+
+    UnicodeString result = ((MeasureFormat *) measure_format)->formatMeasuresPer((const Measure *) measures, measure_count, (const MeasureUnit &) measure_unit, unicode_append_to, (FieldPosition &) field_position, status);
+    const UChar *uchar_result = result.getTerminatedBuffer();
+
+    retv_if(uchar_result == NULL, I18N_ERROR_INVALID_PARAMETER);
+    int32_t ulen = u_strlen(uchar_result);
+
+    retv_if(ulen <= 0, I18N_ERROR_INVALID_PARAMETER);
+    *append_to = (char *) malloc(ulen + 1);
+
+    retv_if(*append_to == NULL, I18N_ERROR_OUT_OF_MEMORY);
+
+    u_austrcpy(*append_to, uchar_result);
+
+    return _i18n_error_mapping(status);
 }
 
 int i18n_measure_format_create_currency_format_from_locale(i18n_measure_format_h *measure_format, const char *language, const char *country)
